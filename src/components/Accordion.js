6d265a2c22f180a9c2c87c2d577fb8d4d1515d9c
Lykoskia <68440961+Lykoskia@users.noreply.github.com>
2024-02-10 18:20:13 +0100

    Update Accordion.js

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -18,7 +18,7 @@ export default function Accordion({ title, children, translations }) {
                 className={`group rounded-full flex justify-between items-center p-3 pl-8 pr-8 cursor-pointer select-none dark:text-gray-300 ${isOpen ? "bg-red-200 dark:bg-sky-900 hover:bg-sky-300 dark:hover:bg-sky-700" : "bg-sky-200 dark:bg-sky-950 hover:bg-sky-200 dark:hover:bg-sky-800"}`}
                 onClick={handleToggle}
             >
-                <span className="text-lg font-bold text-red-700 group-hover:text-sky-700 dark:group-hover:text-white dark:text-sky-300">
+                <span className="text-lg font-bold text-red-700 group-hover:text-sky-700 dark:group-hover:text-white dark:text-sky-300 uppercase">
                     {getTranslatedTitle(title)}
                 </span>
                 <section className="group-hover:text-sky-700 dark:group-hover:text-white">
