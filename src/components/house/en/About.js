b0794c62b521f56374cd53719c5817b1477ffe9a
Lykoskia <68440961+Lykoskia@users.noreply.github.com>
2024-02-11 23:17:22 +0100

    Update About.js

--- src/components/house/en/About.js
+++ src/components/house/en/About.js
@@ -42,9 +42,7 @@ export default function About() {
 
     return (
         <section className="text-black dark:text-sky-200 p-4">
-            <div className={`${showMore ? 'hidden' : 'block'}`}>
-                {previewContent}
-            </div>
+            {previewContent}
             <div className={`${showMore ? 'block' : 'hidden'}`}>
                 {fullContent}
             </div>
